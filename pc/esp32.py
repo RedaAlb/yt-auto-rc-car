0d9363a00d0d0aa3a95783a246b0c459777d0ab3
RedaAlb <reda.alb2@hotmail.com>
2022-11-12 00:14:29 +0000

    Make ESP32 thread to prevent interference with camera stream.

--- pc/esp32.py
+++ pc/esp32.py
@@ -1,4 +1,6 @@
 import socket
+import threading 
+from queue import Queue
 
 
 HOST_IP = "192.168.0.21"  # PC ip address.
@@ -14,6 +16,64 @@ esp32_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 esp32_socket.bind((HOST_IP, ESP32_PORT))
 esp32_socket.settimeout(0.001)
 print(LOG_PREFIX, f"UDP connection opened on {HOST_IP}:{ESP32_PORT}...")
+
+
+# These queues are used to pass data between the main thread (main.py) and the ESP32 thread.
+recv_queue = Queue()  # For data received from the ESP32.
+send_queue = Queue()  # For data to be sent to the ESP32.
+
+
+def start_thread():
+    """Start a thread for the ESP32 communication to not interfere with camera feed (main thread).
+    """
+    esp32_thread = threading.Thread(target=thread_target, args=((recv_queue, send_queue)))
+    esp32_thread.start()
+
+
+def thread_target(recv_queue, send_queue):
+    """The thread target function for this ESP32 module.
+
+    Args:
+        recv_queue (Queue): Used to share data between threads for data received from the ESP32.
+        send_queue (Queue): Used to share data between threads to be sent to the ESP32.
+    """
+    while True:
+        esp32_data_recv = recv_esp32_data()
+        if esp32_data_recv is not None:
+            recv_queue.queue.clear()
+            recv_queue.put(esp32_data_recv)
+
+        if not send_queue.empty():
+            esp32_data_to_send = send_queue.get()
+            
+            # For thread termination.
+            if(esp32_data_to_send == 0):
+                break
+
+            send_data_esp32(esp32_data_to_send)
+
+
+def get_queue_value():
+    """Get value from the thread queue which is the received value from the ESP32.
+
+    Returns:
+        list/None: A list containing all data received, otherwise None if no data was received.
+    """
+    if not recv_queue.empty():
+        esp32_data_recv = recv_queue.get()
+        return esp32_data_recv
+    else:
+        return None
+
+
+def set_queue_value(data):
+    """Set the value of the thread queue to send to the ESP32.
+
+    Args:
+        data (list): A list containing all the data to send to the ESP32.
+    """
+    send_queue.queue.clear()
+    send_queue.put(data)
 
 
 def recv_esp32_data():
@@ -46,4 +106,3 @@ def send_data_esp32(data):
         esp32_socket.sendto(data_bytes, (ESP32_IP, ESP32_PORT))
     except:
         print(LOG_PREFIX, "Failed to send to ESP32, possibly incorrect ip or port.")
-
--- pc/main.py
+++ pc/main.py
@@ -9,28 +9,37 @@ import esp32
 def run():
     """Main function to run on the pc.
     """
+
+    # Using a thread for the ESP32 communication to not interfere with camera feed (this main thread).
+    esp32.start_thread()
+    
     while True:
-        frame = raspberry.get_frame()
+        frame = raspberry.get_frame()  # Get frame from the raspberry pi using UDP connection.
 
         # Skip frame if unsuccesfully received and move straight on to the next frame.
         if frame is None:
             continue
+        
 
-        esp32_recv_data = esp32.recv_esp32_data()
-        if esp32_recv_data is not None:
-            print("ESP32 received data:", esp32_recv_data)
+        # Obtain the data received from the ESP32 using the thread queue.
+        esp32_data_recv = esp32.get_queue_value()
+        if esp32_data_recv is not None:
+            print(esp32_data_recv)
 
-
+        # Send data to the ESP32 using the thread queue.
         import random
-        esp32_data_to_send = [2, 2, 2]  # Sample data.
-        esp32_data_to_send[1] = random.randint(0, 255)
-        esp32.send_data_esp32(esp32_data_to_send)
+        esp32_data_to_send = [1, random.randint(0, 255), 3]  # Sample data.
+        esp32.set_queue_value(esp32_data_to_send)
 
 
         cv2.imshow("Frame", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    
+
+    cv2.destroyAllWindows()
+    esp32.set_queue_value(0)  # To terminate the ESP32 thread.
 
 
 if __name__ == "__main__":
